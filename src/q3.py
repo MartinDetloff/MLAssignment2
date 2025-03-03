a516a5670547a5662ba82a95de591e3f95d55881
adrianfaust <adrianbfaust@gmail.com>
2025-03-02 20:30:09 -0700

    Update plotting settings.

--- src/q3.py
+++ src/q3.py
@@ -12,6 +12,8 @@ training_times = {}
 
 random_seed = 1
 
+log_scales = False
+
 for d in dimensions:
     for n in num_samples:
         X_train, X_test, y_train, y_test = make_classification(d, n, 1.0, random_seed)
@@ -25,26 +27,30 @@ for d in dimensions:
 
 for d in dimensions:
     times = [training_times[(d, n)] for n in num_samples]
-    plt.plot(num_samples, times, label=f"d={d}")
+    plt.plot(num_samples, times, marker="o", label=f"d={d}")
 
 plt.xlabel("Number of Samples")
 plt.ylabel("Training Time")
 plt.title("Training Time vs. Number of Samples")
 plt.legend()
 plt.grid(True)
-plt.xscale('log')
-plt.yscale('log')
+if log_scales:
+    plt.xscale('log')
+    plt.yscale('log')
+plt.savefig("training_time_vs_samples.eps")
 plt.show()
 
 for n in num_samples:
     times = [training_times[(d, n)] for d in dimensions]
-    plt.plot(dimensions, times, label=f"n={n}")
+    plt.plot(dimensions, times, marker="o", label=f"n={n}")
 
 plt.xlabel("Dimensions")
 plt.ylabel("Training Time")
 plt.title("Training Time vs. Dimensions")
 plt.legend()
 plt.grid(True)
-plt.xscale('log')
-plt.yscale('log')
+if log_scales:
+    plt.xscale('log')
+    plt.yscale('log')
+plt.savefig("training_time_vs_dimensions.eps")
 plt.show()
